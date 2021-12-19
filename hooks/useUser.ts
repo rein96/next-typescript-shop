3173d22498aa2aba16e1cd497e3c0e7c98fd55a6
Reinhart Andreas <reinhartandreas96@gmail.com>
2021-12-19 23:55:28 +0700

    refactor: add useSignIn hook

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,7 +1,19 @@
-import { useQuery } from "react-query"
+import { useMutation, useQuery, useQueryClient } from "react-query"
 import { fetchJson } from "lib/api"
+import { User } from "lib/user"
 
-export function useUser() {
+interface SignInVariables {
+  email: string;
+  password: string;
+}
+
+interface UseSignInResult {
+  signInError: boolean;
+  signInLoading: boolean;
+  signIn: (email: string, password: string) => Promise<boolean>;
+}
+
+export function useUser(): User {
   const { data } = useQuery('USER_QUERY', async () => {
     try {
       return await fetchJson('/api/user')
@@ -14,4 +26,38 @@ export function useUser() {
   })
 
   return data;
+}
+
+export function useSignIn(): UseSignInResult {
+  const queryClient = useQueryClient()
+
+  // Will be run from mutateAsync()
+  const { isError, isLoading, mutateAsync } =
+    useMutation<User, Error, SignInVariables>(({ email, password }) =>
+      fetchJson('api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+      }))
+
+  return {
+    signInError: isError,
+    signInLoading: isLoading,
+    signIn: async (email: string, password: string) => {
+      try {
+        // Get user data from useMutation hook
+        const user = await mutateAsync({ email, password })
+
+        // Force update USER_QUERY to log in immediately 
+        queryClient.setQueryData('USER_QUERY', user)
+
+        // Success log in
+        return true
+
+      } catch (error) {
+        // Fail to log in
+        return false
+      }
+    }
+  }
 }
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,39 +1,26 @@
 import React, { useState, FormEventHandler } from 'react';
-import { useMutation, useQueryClient } from 'react-query';
 import { useRouter } from 'next/router';
 import Page from 'components/Page';
 import Field from 'components/Field';
 import Input from 'components/Input';
 import Button from 'components/Button';
-import { fetchJson } from 'lib/api';
+import { useSignIn } from 'hooks/useUser';
 
 const SignInPage: React.FC = () => {
   const router = useRouter();
-  const queryClient = useQueryClient()
+
+  const { signIn, signInError, signInLoading } = useSignIn()
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const { isError, isLoading, mutateAsync } = useMutation(() => fetchJson('api/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
-  }))
-
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
-    console.log('should submit:', { email, password });
     try {
-      // Get user data from useMutation hook
-      const user = await mutateAsync()
+      const isValid = await signIn(email, password)
 
-      // Force update USER_QUERY to log in immediately 
-      queryClient.setQueryData('USER_QUERY', user)
-
-      // Redirect to home
-      router.push('/')
-
-      console.log('handleSubmit: user', user)
+      // Redirect to home 
+      if (isValid) return router.push('/')
 
     } catch (err) {
       // mutation.isError will be true
@@ -58,21 +45,9 @@ const SignInPage: React.FC = () => {
             onChange={(event) => setPassword(event.target.value)}
           />
         </Field>
-        {
-          isError && <p className="text-red-700">
-            Invalid credentials
-          </p>
-        }
-        {
-          isLoading &&
-          <p>Loading...</p>
-        }
-        {
-          !isLoading &&
-          <Button type="submit">
-            Sign In
-          </Button>
-        }
+        {signInError && <p className="text-red-700">Invalid credentials</p>}
+        {signInLoading && <p>Loading...</p>}
+        {!signInLoading && <Button type="submit">Sign In</Button>}
       </form>
     </Page>
   );
